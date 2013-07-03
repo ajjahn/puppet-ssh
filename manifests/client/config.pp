7008e7745d9e54c8be9cd50f9317dc39f5bf07eb
Adam Jahn <ajjahn@gmail.com>
2013-07-03 12:45:57 -0400

    config file should be owned by root

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -2,6 +2,8 @@ class ssh::client::config {
   
   file { "/etc/ssh/ssh_config":
     ensure => present,
+    owner => root,
+    group => root,
     source => $ssh::client::params::ssh_config_source,
     require => Class["ssh::client::install"]
   }
